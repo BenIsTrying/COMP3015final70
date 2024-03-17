945de14034fec5f9dd38532a69d7700a55f6f37c
BenIsTrying <benjamin.george-3@students.plymouth.ac.uk>
2024-03-17 15:31:16 +0000

    testing rotation

--- scenebasic_uniform.h
+++ scenebasic_uniform.h
@@ -6,13 +6,18 @@
 #include <glad/glad.h>
 #include "helper/glslprogram.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+
 class SceneBasic_Uniform : public Scene
 {
 private:
     GLuint vaoHandle;
     GLSLProgram prog;
     float angle;
-
+    glm::mat4 rotationMatrix;
     void compile();
 
 public:
