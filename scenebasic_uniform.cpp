3a34394ae32bb8038daab663395b7ede1ed7dfb7
BenIsTrying <benjamin.george-3@students.plymouth.ac.uk>
2024-03-18 00:50:10 +0000

    penguin texture

--- scenebasic_uniform.cpp
+++ scenebasic_uniform.cpp
@@ -23,7 +23,7 @@ using glm::vec3;
 using glm::mat4;
 
 
-SceneBasic_Uniform::SceneBasic_Uniform() : plane(1.0f,1.0f,100,100) {
+SceneBasic_Uniform::SceneBasic_Uniform() : plane(1.0f, 1.0f, 100, 100) {
     mesh = ObjMesh::load("media/penquin.obj");
 }
 
@@ -59,35 +59,36 @@ void SceneBasic_Uniform::initScene()
 
 
 
-   // prog.setUniform("Light.La", vec3(0.4f, 0.4f, 0.4f));
-    //prog.setUniform("Light.Ls", vec3(1.0f, 1.0f, 1.0f));
+    // prog.setUniform("Light.La", vec3(0.4f, 0.4f, 0.4f));
+     //prog.setUniform("Light.Ls", vec3(1.0f, 1.0f, 1.0f));
 
 
 }
 
 void SceneBasic_Uniform::compile()
 {
-	try {
-		prog.compileShader("shader/basic_uniform.vert");
-		prog.compileShader("shader/basic_uniform.frag");
+    try {
+        prog.compileShader("shader/basic_uniform.vert");
+        prog.compileShader("shader/basic_uniform.frag");
 
-		prog.link();
-		prog.use();
-	} catch (GLSLProgramException &e) {
-		cerr << e.what() << endl;
-		exit(EXIT_FAILURE);
-	}
+        prog.link();
+        prog.use();
+    }
+    catch (GLSLProgramException& e) {
+        cerr << e.what() << endl;
+        exit(EXIT_FAILURE);
+    }
 }
 
-void SceneBasic_Uniform::update( float t )
+void SceneBasic_Uniform::update(float t)
 {
     model = glm::rotate(model, glm::radians(0.6f), vec3(0.0f, 1.0f, 0.1f));
 }
 
 void SceneBasic_Uniform::render()
 {
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 
     prog.setUniform("Material.Kd", vec3(0.2f, 0.55f, 0.9f));
     prog.setUniform("Material.Ka", vec3(0.2f, 0.55f, 0.9f));
@@ -104,15 +105,15 @@ void SceneBasic_Uniform::render()
 
 void SceneBasic_Uniform::resize(int w, int h)
 {
-    glViewport(0,0,w,h);
+    glViewport(0, 0, w, h);
     width = w;
     height = h;
-    
-    projection = glm::perspective(glm::radians(70.0f), (float)w/h, 0.3f, 100.0f);
+
+    projection = glm::perspective(glm::radians(70.0f), (float)w / h, 0.3f, 100.0f);
 }
 
 void SceneBasic_Uniform::setMatrices() {
-    mat4 mv = view*model;
+    mat4 mv = view * model;
     prog.setUniform("ModelViewMatrix", mv);
     prog.setUniform("NormalMatrix", glm::mat3(vec3(mv[0]), vec3(mv[1]), vec3(mv[2])));
     prog.setUniform("MVP", projection * mv);
